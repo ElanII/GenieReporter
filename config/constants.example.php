996b3e2ae7078e4e3b04810a4bca553117048a7f
manuwickrama <manuwickrama@yahoo.com>
2017-04-06 11:37:41 +0930

    Changes to comments

--- config/constants.example.php
+++ config/constants.example.php
@@ -2,6 +2,7 @@
 <?PHP
 /*
 This is the example file containing Config information.
+Adjust the settings for the site and rename the file to 'constants.php'.
  */
 return array(
 'dsn' => '4D:host=10.10.10.500;port=19812;charset=UTF-8',
